0f30ea239235633735066b030533c5f14dc76acb
tillahoffmann <t.hoffmann13@imperial.ac.uk>
2016-11-24 10:38:00 +0000

    Use the current environment for generating previews.

--- jupyter-notebook-quick-look/GenerateHTMLForJupyter.m
+++ jupyter-notebook-quick-look/GenerateHTMLForJupyter.m
@@ -15,11 +15,11 @@ NSString * GenerateHTMLForJupyter(NSURL * notebook_url) {
     NSStringEncoding enc;
     NSString * content = [NSString stringWithContentsOfFile:notebook_path usedEncoding:&enc error:NULL];
     NSString * digest = [content MD5Hash];
-    list.launchPath = @"/usr/local/bin/python3";
+    list.launchPath = @"/usr/bin/env";
     list.environment = [NSDictionary dictionaryWithObjectsAndKeys:@"en_US.UTF-8", @"LC_ALL", nil];
     list.currentDirectoryPath = @"/";
     NSString * out_path = [NSString stringWithFormat:@"/tmp/%@.html",digest];
-    list.arguments = @[@"-m", @"nbconvert", @"--to", @"html", notebook_path, @"--output", out_path];
+    list.arguments = @[@"python", @"-m", @"nbconvert", @"--to", @"html", notebook_path, @"--output", out_path];
     if (![[NSFileManager defaultManager] fileExistsAtPath:out_path]) {
         [list launch];
         [list waitUntilExit];
